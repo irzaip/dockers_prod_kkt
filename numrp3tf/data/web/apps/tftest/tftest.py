4c2ea85c812adbed72a821b3ce25fca36ade6d57
imot <bayu.irian@gmail.com>
2018-11-09 23:47:26 +0700

    .gitignore fix

--- numrp3tf/data/web/apps/tftest/tftest.py
+++ numrp3tf/data/web/apps/tftest/tftest.py
@@ -1,53 +1,118 @@
-#from flask import Flask, request
-from pprint import pprint
-import uwsgi
-import os
+#!/usr/bin/env python
+from threading import Lock
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
 
-#app = Flask(__name__)
-#app.debug = True
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
 
-#@app.route('/')
-#def index():
-#	return "<span style='color:red'>Hello from flask (uwsgi)</span>"
-
-def xsendfile(e, sr):
-    sr('200 OK', [('Content-Type', 'image/png'), ('X-Sendfile', os.path.abspath('logo_uWSGI.png'))])
-    return b''
-
-
-def serve_logo(e, sr):
-    sr('200 OK', [('Content-Type', 'image/png')])
-    return uwsgi.sendfile('logo_uWSGI.png')
+app = Flask(__name__)
+application = app
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 
 
-def serve_config(e, sr):
-    sr('200 OK', [('Content-Type', 'text/html')])
-    for opt in uwsgi.opt.keys():
-        body = "{opt} = {optvalue}<br/>".format(opt=opt, optvalue=uwsgi.opt[opt].decode('ascii'))
-        yield bytes(body.encode('ascii'))
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
 
-routes = {}
-routes['/tftest/xsendfile'] = xsendfile
-routes['/tftest/logo'] = serve_logo
-routes['/tftest/config'] = serve_config
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+@app.route('/header')
+def index2():
+    print(request.environ)
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
 
 
-def application(env, start_response):
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
 
-    if env['PATH_INFO'] in routes:
-        return routes[env['PATH_INFO']](env, start_response)
 
-    start_response('200 OK', [('Content-Type', 'text/html')])
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
 
-    body = """
-<img src="/tftest/logo"/> version {version}<br/>
-<hr size="1"/>
 
-Configuration<br/>
-<iframe src="/tftest/config"></iframe><br/>
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
 
-<br/>
 
-    """.format(version=uwsgi.version.decode('ascii'))
+@socketio.on('close_room', namespace='/test')
+def close(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
+                         'count': session['receive_count']},
+         room=message['room'])
+    close_room(message['room'])
 
-    return bytes(body.encode('ascii'))
+
+@socketio.on('my_room_event', namespace='/test')
+def send_room_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         room=message['room'])
+
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
